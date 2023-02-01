791f57f6253d62a67fbcd4b2d99fe42edfd57be3
josefaidt <josef.aidt@gmail.com>
2023-01-31 18:35:35 -0800

    optional resolve path argument

--- packages/project/src/resolve.ts
+++ packages/project/src/resolve.ts
@@ -1,7 +1,7 @@
 import { readPackageJson } from './packageJson.js'
 import { createProject } from './Project.js'
 
-export async function resolve(path: string) {
+export async function resolve(path?: string) {
   const pkg = await readPackageJson(path)
   const project = createProject(pkg)
   return project
